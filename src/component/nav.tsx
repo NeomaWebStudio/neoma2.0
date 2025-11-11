afc4c2ade0eb4512b9a4351be9c5c7c8ae5290ff
Taras M <tmaliukh@gmail.com>
2025-11-11 09:52:59 +0200

    set isMenuOpen

--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -59,7 +59,7 @@ const Nav = ({ translations }: TranslationProps) => {
       )}
 
       {/* Side Menu */}
-      <div className={`fixed top-0 left-0 h-full w-85 bg-[#05040D] z-50 transform transition-transform duration-300 ease-in-out ${true ? 'translate-x-0' : '-translate-x-full'
+      <div className={`fixed top-0 left-0 h-full w-85 bg-[#05040D] z-50 transform transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'
         }`}>
         <div className="flex flex-col h-full">
           {/* Menu Header */}
