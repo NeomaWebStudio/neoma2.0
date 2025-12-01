84b3058ce21db769a9ebbea0c720880e9f85f1d2
Taras M <tmaliukh@gmail.com>
2025-12-01 21:30:55 +0200

    Add logo spin

--- src/component/about.tsx
+++ src/component/about.tsx
@@ -7,10 +7,12 @@ interface TranslationProps {
 const About = ({ translations }: TranslationProps) => {
 	return (
 		<div id='about' className='xs:block mb-24'>
-			<h2 className="text-[28px] md:text-4xl lg:text-5xl text-center md:mb-6 text-white py-12 font-nutino">{translations["about_us"]}</h2>
+			<h2 className="text-[28px] md:text-4xl lg:text-5xl text-center text-white font-nutino">{translations["about_us"]}</h2>
 			<div className='flex flex-col md:flex-row gap-4 md:gap-20 '>
 				<div className='flex items-center md:flex-1'>
-					<img src="/Logo3D.svg" alt="" className='' />
+					<video autoPlay loop muted playsInline>
+						<source src="/Logo_spin.webm" type="video/webm" />
+					</video>
 				</div>
 				<p className='text-white text-4 font-nunito md:flex-1 xl:self-center'
 					dangerouslySetInnerHTML={{ __html: translations["about_us_text"] }} />
