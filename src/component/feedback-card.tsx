e42b590d1145211f185a1f7a2bac88637a558724
Taras M <tmaliukh@gmail.com>
2025-12-04 20:00:33 +0200

    Add stars on feedback card

--- src/component/feedback-card.tsx
+++ src/component/feedback-card.tsx
@@ -16,6 +16,22 @@ const FeedbackCard = ({ author, subTitle, feedbackText, image, authorImg }: Feed
         <div className="border-b border-gray-300 w-full mb-4"></div>
         <div className='mb-4'>{subTitle}</div>
         <div className="border-b border-gray-300 w-full mb-6"></div>
+
+        {/* 5-star rating */}
+        <div className="flex gap-1 mb-6">
+          {[...Array(5)].map((_, index) => (
+            <svg
+              key={index}
+              className="w-6 h-6"
+              fill="#FFA157"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
+            </svg>
+          ))}
+        </div>
+
         <div>{feedbackText}</div>
       </div>
 
