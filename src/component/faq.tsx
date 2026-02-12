11eebafbd8b96f42b512ba99041e09744c2a1cd6
Taras M <tmaliukh@gmail.com>
2026-02-12 11:21:46 +0200

    fix scroll on sphere on mobile

--- src/component/faq.tsx
+++ src/component/faq.tsx
@@ -56,19 +56,19 @@ export default function Faq({ translations }: TranslationProps) {
 
   return (
     <div id='faq' className="mb-24">
-      <h3 className="text-center text-[28px] md:text-5xl text-white font-nanito lg:mb-12">Faq</h3>
+      <h3 className="text-center text-[28px] md:text-5xl text-white font-nanito mb-12">Faq</h3>
       <div className="flex flex-col lg:flex-row lg:justify-between gap-12">
         <div
-  className="mt-[40px] md:mt-[150px]"
-  onWheelCapture={(e) => {
-    e.stopPropagation();   // дає сторінці нормальний скрол
-  }}
->
-  <Spline
-    scene="/sphere.splinecode"
-    className="flex justify-center items-center"
-  />
-</div>
+          className="w-full touch-pan-y"
+          onWheelCapture={(e) => {
+            e.stopPropagation();   // дає сторінці нормальний скрол
+          }}
+        >
+          <Spline
+            scene="/sphere.splinecode"
+            className="flex justify-center items-center"
+          />
+        </div>
 
         <div
           id="accordion"
