67cfbaeadfe98a7a4915e267b895e291213bd14a
Taras M <tmaliukh@gmail.com>
2026-02-12 09:19:58 +0200

    Create a background gradiend with keyframes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import AnimatedWaves from '@/component/animatedWaves'
 import AOSInit from '@/component/aos-init'
 import 'aos/dist/aos.css'
 import localFont from 'next/font/local'
+import { BackgroundGradient } from '@/component/background-gradient';
 
 const kurale = Kurale({
   subsets: ['latin', 'cyrillic'],
@@ -47,7 +48,8 @@ export default function RootLayout({
         {' '}
         {/* ${kurale.className} ${geistMono.variable} ${nunito.className} ${merriweather.className} */}
         <AOSInit />
-        <NeatGradientComp />
+        {/* <NeatGradientComp /> */}
+        <BackgroundGradient />
         {/* <AnimatedWaves /> */}
         {children}
       </body>
